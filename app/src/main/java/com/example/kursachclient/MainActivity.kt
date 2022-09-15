55b94a267290bba5edc8a1090d93bd70129b508b
Денис Скурат <den.skur2016@yandex.ru>
2022-09-15 11:36:56 +0300

    Add init commit

---
+++ app/src/main/java/com/example/kursachclient/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.kursachclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.kursachclient.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        val bottomNavigationView = binding.bottomNavigation
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
